fe7afda4fe73d815cbfbc1aa217c0e8297824f2c
bernardo Peña <bernardpena@gmail.com>
2024-03-18 05:31:01 -0300

    Inicio Repositorio

---
+++ assets/js/alquiler.js
@@ -0,0 +1,11 @@
+const alquiler = [ {
+    nombre: 'Apartamento de lujo en zona exclusiva',
+    src: '',
+    descripcion: 'Este apartamento de lujo está ubicado en una exclusiva zona residencial',
+    ubicacion: '123 Luxury Lane, Prestige Suburb, CA 45678',
+    habitaciones: 4,
+    baños: 4,
+    costo: 5000,
+    smoke: false,
+    pets: true }
+]
---
+++ assets/js/propiedades.js
@@ -0,0 +1,11 @@
+const venta1 = [ {
+    nombre: 'Apartamento de lujo en zona exclusiva',
+    src: 'https://fotos.perfil.com/2018/09/21/trim/950/534/nueva-york-09212018-366965.jpg',
+    descripcion: 'Este apartamento de lujo está ubicado en una exclusiva zona residencial',
+    ubicacion: '123 Luxury Lane, Prestige Suburb, CA 45678',
+    habitaciones: 4,
+    baños: 4,
+    costo: 5000,
+    smoke: false,
+    pets: true }
+]
